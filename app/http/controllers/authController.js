54a5dbc13ceefb42fe5e00698f8683c19a39134c
shatrudhan <shatrudhan080@gmail.com>
2021-11-06 20:08:15 +0530

    login using passport

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,10 +1,77 @@
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+const passport = require('passport')
+
+
 function authController() {
     return {
         login(req, res) {
             res.render('auth/login');
         },
+        postLogin(req, res, next) {
+            const { email, password } = req.body
+                // validation
+            if (!email || !password) {
+                req.flash('error', 'All fileds are requires')
+                return res.redirect('/login')
+            }
+            passport.authenticate('local', (err, user, info) => {
+                if (err) {
+                    req.flash('error', info.message)
+                    return next(err)
+                }
+                if (!user) {
+                    req.flash('error', info.message)
+                    return res.redirect('/login')
+                }
+                req.login(user, (err) => {
+                    if (err) {
+                        req.flash('error', info.message)
+                        return next(err)
+                    }
+                    return res.redirect('/')
+                })
+            })(req, res, next)
+        },
         register(req, res) {
             res.render('auth/register');
+        },
+        async postRegister(req, res) {
+            const { name, email, password } = req.body
+                // validation
+            if (!name || !email || !password) {
+                req.flash('error', 'All fileds are requires')
+                req.flash('name', name)
+                req.flash('email', email)
+                return res.redirect('/register')
+            }
+            // check email exitst
+            User.exists({ email: email }, (err, result) => {
+                    if (result) {
+                        req.flash('error', 'Email allready exist')
+                        req.flash('name', name)
+                        req.flash('email', email)
+                        return res.redirect('/register')
+                    }
+                })
+                // has password
+            const haspass = await bcrypt.hash(password, 10)
+                // create user
+            const user = new User({
+                name,
+                email,
+                password: haspass
+            })
+            user.save().then((user) => {
+                return res.redirect('/')
+            }).catch(err => {
+                req.flash('error', 'Somthing Went Wrong')
+                return res.redirect('/register')
+            })
+        },
+        logout(req, res) {
+            req.logout()
+            return res.redirect('/login')
         }
 
     }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 const app = express();
 // database connection
@@ -14,28 +15,37 @@ const url = 'mongodb://localhost:27017/pizza';
 mongoose.connect(url, {});
 const connection = mongoose.connection;
 connection.once('open', () => {
-        console.log('Database Connected..');
-    })
-    //session Store
+    console.log('Database Connected..');
+})
+
+//session Store
 const mongoStore = new MongoDbStore({
         mongooseConnection: connection,
         collection: 'sessions'
     })
     // session config
 app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    store: mongoStore,
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }
-}))
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        store: mongoStore,
+        saveUninitialized: false,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 }
+    }))
+    // passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+    // flash notyfication
 app.use(flash())
     //assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
     //global middleware
 app.use((req, res, next) => {
         res.locals.session = req.session
+        res.locals.user = req.user
         next()
     })
     // set Layout
